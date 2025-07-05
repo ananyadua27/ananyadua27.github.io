21cf714ffcc9e0cb1caf7a837b45ab801db325dd
Ananya Dua <ananyadua@berkeley.edu>
2025-07-05 06:27:03 -0700

    index.html and travel underway

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const port = 8000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/#ananyadua', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')));
-app.get('/research', (req, res) => res.sendFile(path.join(__dirname, 'views/research.html')));
-app.get('/portfolio', (req, res) => res.sendFile(path.join(__dirname, 'views/portfolio.html')));
-app.get('/reflections', (req, res) => res.sendFile(path.join(__dirname, 'views/reflections.html')));
-app.get('/bookshelf', (req, res) => res.sendFile(path.join(__dirname, 'views/bookshelf.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')));
+app.get('/research.html', (req, res) => res.sendFile(path.join(__dirname, 'views/research.html')));
+app.get('/portfolio.html', (req, res) => res.sendFile(path.join(__dirname, 'views/portfolio.html')));
+app.get('/travel.html', (req, res) => res.sendFile(path.join(__dirname, 'views/travel.html')));
+app.get('/bookshelf.html', (req, res) => res.sendFile(path.join(__dirname, 'views/bookshelf.html')));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
