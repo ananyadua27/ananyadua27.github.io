5dd0a6b5742b50f72de5db43babb9de69e47b46c
Ananya Dua <ananyadua@berkeley.edu>
2026-05-28 01:07:21 -0700

    content

---
+++ index.js
@@ -0,0 +1,7 @@
+(() => {
+  const year = new Date().getFullYear().toString();
+
+  document.querySelectorAll("[data-current-year]").forEach((element) => {
+    element.textContent = year;
+  });
+})();
